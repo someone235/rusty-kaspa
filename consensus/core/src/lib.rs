e62907296de51fb19ff0026fb4e3f585a6518029
Ori Newman <orinewman1@gmail.com>
2022-10-30 18:49:37 +0200

    Add apply_proof

--- consensus/core/src/lib.rs
+++ consensus/core/src/lib.rs
@@ -19,6 +19,7 @@ pub mod sign;
 pub mod subnets;
 pub mod tx;
 pub mod utxo;
+pub mod pruning;
 
 /// Integer type for accumulated PoW of blue blocks. We expect no more than
 /// 2^128 work in a single block (btc has ~2^80), and no more than 2^64
