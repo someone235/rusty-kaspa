f398de5cfcca6ce1bc535da6731c320317c0b63e
Ori Newman <orinewman1@gmail.com>
2023-04-13 17:17:22 +0300

    Add log4rs

--- core/src/log.rs
+++ core/src/log.rs
@@ -23,15 +23,39 @@ cfg_if::cfg_if! {
 
 #[cfg(not(target_arch = "wasm32"))]
 pub fn init_logger(filters: &str) {
-    let level = log::LevelFilter::Info;
+    use log4rs::{
+        append::{
+            console::{ConsoleAppender, Target},
+            file::FileAppender,
+        },
+        config::{Appender, Root},
+        encode::pattern::PatternEncoder,
+        filter::threshold::ThresholdFilter,
+        Config,
+    };
 
-    env_logger::Builder::new()
-        .format_target(false)
-        .format_timestamp_secs()
-        .filter_level(level)
-        .parse_default_env()
-        .parse_filters(filters)
-        .init();
+    let level = log::LevelFilter::Info;
+    let file_path = "/tmp/foo.log";
+
+    // Build a stderr logger.
+    let stderr = ConsoleAppender::builder().target(Target::Stderr).build();
+
+    // Logging to log file.
+    let logfile = FileAppender::builder()
+        // Pattern: https://docs.rs/log4rs/*/log4rs/encode/pattern/index.html
+        .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
+        .build(file_path)
+        .unwrap();
+
+    // Log Trace level output to file where trace is the default level
+    // and the programmatically specified level to stderr.
+    let config = Config::builder()
+        .appender(Appender::builder().filter(Box::new(ThresholdFilter::new(level))).build("logfile", Box::new(logfile)))
+        .appender(Appender::builder().filter(Box::new(ThresholdFilter::new(level))).build("stderr", Box::new(stderr)))
+        .build(Root::builder().appender("logfile").appender("stderr").build(LevelFilter::Trace))
+        .unwrap();
+
+    log4rs::init_config(config).unwrap();
 
     workflow_log::set_log_level(level);
 }
